d71faeb1a88e48f79cc4c91ecce8f01ab7977ca6
Jasmine McManus <jasmine.mcmanus@outlook.com>
2023-07-24 20:10:46 +1000

    done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+var customerName = 'bob';
+const leastFavoriteCustomer = 'definitely bob';
+
+console.log(customerName);
+console.log(typeof customerName);
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+};
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+    return bestCustomer;
+};
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+};
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'not bob actually';
+};
+
+changeLeastFavoriteCustomer();
